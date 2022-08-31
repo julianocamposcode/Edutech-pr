0927a2902f3eae2a440aca35e6ec23f6c7d2b652
Juliano Campos <88381643+Julianocamposdossantos@users.noreply.github.com>
2022-08-31 14:02:49 -0300

    Add files via upload

---
+++ Trabalho Coringa/coringa.js
@@ -0,0 +1,8 @@
+			const btn = document.querySelector('.btn-mobile');
+			const nav = document.querySelector('.bottom');
+
+
+			btn.addEventListener('click' , () => {
+				nav.classList.toggle('ativar');
+			});
+
